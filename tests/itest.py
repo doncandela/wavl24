9cd1cbe17a46f31e173a7f9d8734d208233c57f9
doncandela <candela@physics.umass.edu>
2024-02-02 16:01:53 -0500

    Added Wavelet and Daub4 docstrings

--- tests/itest.py
+++ tests/itest.py
@@ -1,7 +1,6 @@
-"""A test module for the wavl24 package.
+"""A test program for the wavl24 package.
 
 itest.py simple tests that wavl24 can be imported.
-
 """
 THIS_IS = 'itest.py 2/2/24 D. Candela'
 
@@ -10,3 +9,9 @@ from wavl24 import wavl
 if __name__=='__main__':
     print(f'This is: {THIS_IS}')
     print(f'Using: {wavl.THIS_IS}')
+    
+    # Print Daub4 coefficients.
+    print(wavl.D4C0)
+    print(wavl.D4C1)
+    print(wavl.D4C2)
+    print(wavl.D4C3)
--- wavl24/wavl.py
+++ wavl24/wavl.py
@@ -7,3 +7,58 @@ pywavelets (not used here) is a much more complete and advanced wavele
 package than this module.
 """
 THIS_IS = 'wavl.py 2/2/24 D. Candela'
+
+import numpy as np
+from numpy import sqrt
+
+""" **********************************************************************
+      FUNCTIONS
+************************************************************************** """
+def wt1(a,wlet,forward=True):
+    """One-dimensional discrete wavelet transform.  Adapted from NR3 Sec.
+    13.10.2
+    
+    Parameters
+    ----------
+    a : array (n) of float
+        Input array, which this function replaces by its wavelet transform
+        (if forward is true) or inverse transform (otherwise). n must be a
+        power of 2.
+    wlet: Wavelet
+        Wavelet type to use.
+    forward : bool
+    """
+
+
+""" **********************************************************************
+      BASE CLASSES
+************************************************************************** """
+class Wavelet:
+    """Base class for wavelets. Adapted from NR3 Sec. 13.10.2
+    """
+    def filt(a,n,forward):
+        """Applies wavelet filter (if forward is true) or its transpose
+        (otherwise) to a[0..n-1]. Called hierarchically by wt1 and wtn.
+        """
+        
+    def condition(a,n,isign):
+        """Pre-conditioning and post-conditioning.
+        """
+
+""" **********************************************************************
+      CLASSES FOR SPECIFIC WAVELET TYPES
+************************************************************************** """
+# Daubechie-4 coefficients
+D4D = 4*sqrt(2)
+D4C0 = (1+sqrt(3))/D4D
+D4C1 = (3+sqrt(3))/D4D
+D4C2 = (3-sqrt(3))/D4D
+D4C3 = (1-sqrt(3))/D4D
+
+class Daub4(Wavelet):
+    """Daubechie 4-coefficient wavelet.
+    """
+    def filt(a,n,forward):
+        """Applies wavelet filter (if forward is true) or its transpose
+        (otherwise) to a[0..n-1]. Called hierarchically by wt1 and wtn.
+        """
