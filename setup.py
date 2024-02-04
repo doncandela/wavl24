d54017c4320e2e0b759bcbdfdc16836fa97b62a5
doncandela <candela@physics.umass.edu>
2024-02-04 18:38:23 -0500

    move package to subdirectory src

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-"""setup.py for wavl24  2/2/24  D.C.
+"""setup.py for wavl24  2/4/24  D.C.
 """
 from setuptools import setup
 
 setup(name='wavl24',
       version='0.0',
+      package_dir = {'': 'src'},
       packages=['wavl24'],
       license = 'MIT',
       classifiers = [
